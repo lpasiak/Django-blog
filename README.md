d26ed16970a61289989b1c4e214f5ceb0dd91c7c
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2024-12-05 17:30:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello!
+
+This is my first project created in Django. It's just a simple blog.
---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # on_delete - We want to import user's posts if a User is deleted
+
+    def __str__(self):
+        return self.title
