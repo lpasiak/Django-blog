11833908dc581c97e945118849f8120dd6f69e16
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2024-12-06 00:33:11 +0100

    Fix email field. Now it has to be unique

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
