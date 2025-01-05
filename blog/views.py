3643de96b25230ad63dcfb3a35084db84e4c0f6a
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-01-05 22:57:01 +0100

    Add Detail class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
